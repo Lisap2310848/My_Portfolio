113e36a008b90cd000a0446923bc571dce3053dd
NEZLIOUI LISA p2310848 <lisa.nezlioui@etu.univ-lyon1.fr>
2026-03-20 17:44:20 +0100

    Sauvegarde avant nettoyage de l'historique

--- script.js
+++ script.js
@@ -25,3 +25,25 @@
   }, 1000);
 }
 });
+
+const zooms=document.querySelectorAll(".zoomable");
+zooms.forEach((zoom),
+  {
+    zoom.addEventListener("mouseover",(e)=>
+    {
+      // zoom externe au centre de la page ok?
+      const rect=zoom.getBoundingClientRect();//position de l'element
+      const centerX=rect.left + rect.width/2;// coordonee actuel x du centre de mon image
+      const centerY=rect.top+ rect.height/2;//coordonee y actuel du centre de mon image 
+      
+      const offsetX=window.innerWidth/2 -centerX;
+      const offsetwindow=window.innerHeight/2 -centerY;
+
+      zoom.style.transform=`translate(${offsetX}px,${offsetY}px)scale(1.4)`;
+       zoom.style.width="400px";
+       zoom.style.height="400px";
+    }
+  );
+  
+  }
+);
